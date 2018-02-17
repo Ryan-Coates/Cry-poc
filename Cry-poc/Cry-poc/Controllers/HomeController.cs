2ee8cae026e856939fac23f363dff3d4cdd8e749
Ryan-Coates <Ryan.Coates@tombola.com>
2018-02-17 16:38:19 +0000

    initial commit basic google maps setup

---
+++ Cry-poc/Cry-poc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cry_poc.Models;
+
+namespace Cry_poc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MapsModel model = new MapsModel();
+            model.Points = new List<MapPoint>();
+            model.Points.Add(new MapPoint
+            {
+                Id = 1,
+                PlaceName = "Zaghouan",
+                GeoLong = "36.401081",
+                GeoLat = "10.16596"
+            });
+            model.MapPointsJSON = Newtonsoft.Json.JsonConvert.SerializeObject(model.Points);
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cry-poc/Cry-poc/Models/ErrorViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cry_poc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class MapsModel
+    {
+        public List<MapPoint> Points { get; set; }
+        public string MapPointsJSON { get; set; }
+
+
+    }
+
+    public class MapPoint
+    {
+        //"Id": 1, "PlaceName": "Zaghouan", "GeoLong": "36.401081", "GeoLat": "10.16596"
+        public int Id { get; set; }
+        public string PlaceName { get; set; }
+        public string GeoLong { get; set; }
+        public string GeoLat { get; set; }
+
+    }
+}
